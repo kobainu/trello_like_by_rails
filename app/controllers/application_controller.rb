bc42f5a7454a753051e0c2339d31b731f64560a5
小林大志 <kobayashihiroshi@kobayashihiroshinoMacBook-Air.local>
2022-12-15 23:05:53 +0900

    add sign-out link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  # 全てのコントローラーを実行する前にauthenticate_user!を読み込み(authenticate_user! => サインインしていない状態でアクセスしようとすると、サインインページにリダイレクトされます)
+  before_action :authenticate_user!
 end
